7be346cbb0a0d84468a466e3cbc858dd53a39606
Jason Luck <jason.luck@icfi.com>
2017-01-31 17:17:44 -0500

    Making sure Liquibase container waits for DB

--- compose/db/run-liquibase.sh
+++ compose/db/run-liquibase.sh
@@ -2,6 +2,21 @@
 
 export LIQUIBASE_HOME=/opt/liquibase
 export DB_URL=jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}
+export DB_NOT_READY="true"
+
+while [ ! -z "${DB_NOT_READY}" ]
+do
+	echo "Waiting 5sec for DB to start..."
+	sleep 5
+	$LIQUIBASE_HOME/liquibase \
+	--driver=com.mysql.jdbc.Driver \
+	--url=$DB_URL \
+	--username=$DB_USER \
+	--password=$DB_PASSWORD \
+	--changeLogFile=$CHANGELOG \
+	listLocks > /tmp/db_ready 2>&1
+	DB_NOT_READY=$(cat /tmp/db_ready | grep "Unexpected error running Liquibase: com.mysql.jdbc.exceptions.jdbc4.CommunicationsException: Communications link failure")
+done
 
 $LIQUIBASE_HOME/liquibase \
 	--driver=com.mysql.jdbc.Driver \
