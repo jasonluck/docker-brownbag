83f344e0f1d62e77d4ddb3663fdee3a2082f9211
Jason Luck <jason.luck@icfi.com>
2017-02-03 15:01:36 -0500

    Adding reverse-proxy for services api

--- compose/web/content/js/app.js
+++ compose/web/content/js/app.js
@@ -1,6 +1,30 @@
 
-//Enable tooltips for bootstrap components 
-$(function () {
-    'use strict';
-    $('[data-toggle="tooltip"]').tooltip();
-});
+
+function getGuestRegistry() {
+    $.ajax({
+        url: "/services/guest",
+        type: "GET",
+        contentType: 'application/json; charset=utf-8',
+        success: function(data) {
+            for (var i = 0; i < data.guest.length; i++) {
+                var guest = data.guest[i];
+                    $("#registry").find('tbody')
+                        .append($('<tr>')
+                            .append($('<td>')
+                                .text(guest.date)
+                            )
+                            .append($('<td>')
+                                .text(guest.firstname)
+                            )
+                            .append($('<td>')
+                                .text(guest.lastname)
+                            )
+                        )
+            }
+        },
+        error : function(jqXHR, textStatus, errorThrown) {
+        },
+    });
+}
+
+$(document).ready( getGuestRegistry() );
