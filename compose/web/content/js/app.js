1a3bad625c311388a2a543d6b18078eb21ada261
Jason Luck <jason.luck@icfi.com>
2017-02-06 17:26:38 -0500

    Adding js to add guest

--- compose/web/content/js/app.js
+++ compose/web/content/js/app.js
@@ -5,26 +5,71 @@ function getGuestRegistry() {
         url: "/services/guest",
         type: "GET",
         contentType: 'application/json; charset=utf-8',
-        success: function(data) {
+        success: function (data) {
+            var tableBody = $("#registry").find('tbody');
+            //Clear table data
+            tableBody.empty();
+
+            //Add rows to table
             for (var i = 0; i < data.guest.length; i++) {
                 var guest = data.guest[i];
-                    $("#registry").find('tbody')
-                        .append($('<tr>')
-                            .append($('<td>')
-                                .text(guest.time)
-                            )
-                            .append($('<td>')
-                                .text(guest.firstname)
-                            )
-                            .append($('<td>')
-                                .text(guest.lastname)
-                            )
+                tableBody
+                    .append($('<tr>')
+                        .append($('<td>')
+                            .text(guest.time)
                         )
+                        .append($('<td>')
+                            .text(guest.firstname)
+                        )
+                        .append($('<td>')
+                            .text(guest.lastname)
+                        )
+                    )
             }
         },
-        error : function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
         },
     });
 }
 
-$(document).ready( getGuestRegistry() );
+function addGuest(data) {
+    $.ajax({
+        url: "services/guest",
+        type: "POST",
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(data),
+        success: function () {
+            $("#sign-form :input").each(function() {
+                var input = $(this);
+                
+                if (input.attr('name')) {
+                   input.val("")
+                }
+            })
+            $('#sign-submit-button').removeClass('btn-danger')
+            $('#sign-submit-button').addClass('btn-success')
+            getGuestRegistry();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            $('#sign-submit-button').removeClass('btn-success')
+            $('#sign-submit-button').addClass('btn-danger')
+        },
+    })
+}
+
+$(document).ready(getGuestRegistry());
+
+$("#sign-form").submit(function (event) {
+    // stop the regular form submission
+    event.preventDefault();
+
+    var data = {};
+    $("#sign-form :input").each(function() {
+        var input = $(this);
+        
+        if (input.attr('name')) {
+            data[input.attr('name')] = input.val();
+        }
+    })
+    addGuest(data);
+});
